676e8b3f03c5a156a19141bf37697f365f6902cd
Shivam Yadav <shivam.yadav@rapipay.com>
2023-09-11 17:35:22 +0530

    fifteh commit

---
+++ TestingAppFramework/TestingAppFramework.docc/Resources/TestFile.swift
@@ -0,0 +1,18 @@
+//
+//  TestFile.swift
+//  
+//
+//  Created by Shivam Yadav on 11/09/23.
+//
+
+import Foundation
+
+public class functionTest() {
+    
+    public func abcd() {
+        print("abcd..........")
+    }
+    private func efgh() {
+        print("efgh..........")
+    }
+}
