cb27956f5b798830f941b479b3f9a9e30b89a2a8
Shivam Yadav <shivam.yadav@rapipay.com>
2023-09-11 12:22:35 +0530

    second  commit

---
+++ ios.xcarchive/Products/Library/Frameworks/TestingAppFramework.framework/Headers/TestingAppFramework.h
@@ -0,0 +1,18 @@
+//
+//  TestingAppFramework.h
+//  TestingAppFramework
+//
+//  Created by Shivam Yadav on 11/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestingAppFramework.
+FOUNDATION_EXPORT double TestingAppFrameworkVersionNumber;
+
+//! Project version string for TestingAppFramework.
+FOUNDATION_EXPORT const unsigned char TestingAppFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingAppFramework/PublicHeader.h>
+
+
